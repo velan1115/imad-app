90c497a2062bfd4b23e6031d543a7d7b746c87a4
velan1115 <30657111+velan1115@users.noreply.github.com>
2018-02-17 07:26:01 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+//alert
+alert("welcome to IMAD");
+
+//changing main text
+var t = document.getElementById("text");
+t.innerHTML = "Welcome MK!!!";
+
+//image moving
+var image = document.getElementById("madi");
+image.onclick = function(){
+    image.style.marginLeft = "200px";
+};
