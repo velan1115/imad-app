75649099ff74fb9568be89bcecdb04198f798934
velan1115 <30657111+velan1115@users.noreply.github.com>
2018-02-16 23:57:28 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article1',function(req,res){
+    res.sendFile('this article 1');
+});
+app.get('/article2',function(req,res){
+    res.sendFile('this article 2');
+});
+app.get('/article3',function(req,res){
+    res.sendFile('this article 3');
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
