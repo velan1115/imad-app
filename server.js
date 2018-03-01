be14e39508334961de33bac6b4ac1bd108441ddc
velan1115 <30657111+velan1115@users.noreply.github.com>
2018-03-01 03:00:13 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config ={
+  user:'muthukannanksv',
+  datbase:'muthukannanksv',
+  host:'db.imad.hasura-app.io',
+  port:'5432',
+  password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -100,6 +109,23 @@ app.get('/', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+var pool  = new Pool(config);
+
+//db connection
+app.get('test-db', function (req, res) {
+ pool.query('select * from test', function(err,result)
+ {
+     if(err)
+     {
+         res.status(500).send(err.toString());
+     }
+     else
+     {
+         res.send(JSON.stringify(result.rows));
+     }
+ });
 });
 
 var counter = 0;
